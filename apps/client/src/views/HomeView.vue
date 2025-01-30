47cb8ae4e79df2a14b5123677fc4fe3dc497f1e1
Kaio-Cesar <git@kcesar.dev>
2025-01-30 14:14:36 -0300

    Tailwind v4 setup

--- apps/client/src/views/HomeView.vue
+++ apps/client/src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Hello World</div>
+  <div class="w-[100svw] h-[100svh] flex justify-center items-center text-4xl">
+    Hello World
+  </div>
 </template>
