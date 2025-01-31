8c8885cb5167900865c3da6d7d2c0eedb101d689
Kaio-Cesar <git@kcesar.dev>
2025-01-31 00:35:27 -0300

    UI lib setup

--- apps/client/src/views/HomeView.vue
+++ apps/client/src/views/HomeView.vue
@@ -1,7 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { HelloWorld } from 'ideais-ui-kit'
+</script>
 
 <template>
   <div class="w-[100svw] h-[100svh] flex justify-center items-center text-4xl">
-    Hello World
+    <HelloWorld />
   </div>
 </template>
